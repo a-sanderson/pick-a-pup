3097514bac5c52c861cb69b40320ff59334a5d76
Aaron Sanderson <ronsanderson09@gmail.com>
2020-02-01 16:54:24 -0700

    margins nav

--- src/Headerlarge.js
+++ src/Headerlarge.js
@@ -5,16 +5,16 @@ const Headerlarge = props => {
 
     return (
         <div className="headerContainer largeHeader">
-            <div style={{display:"flex",alignItems: "center", justifyContent: "space-evenly"}}>
+            <div style={{position: "relative", left: 30, display:"flex",alignItems: "center" }}>
                 <div className="imageContainer">
                     <img alt="logo" src={logo} className="imageFit"></img>
                 </div>
-                <h1>Pick-a-Pup</h1>
+                <h1 style={{marginLeft:30}}>Pick-a-Pup</h1>
             </div>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/adopt">Adopt</Link>
-            <Link style={{marginRight:10}} to="/info">Breed Info</Link>
+            <Link style={{marginRight:30}} to="/info">Breed Info</Link>
         </div>
     )
 }
